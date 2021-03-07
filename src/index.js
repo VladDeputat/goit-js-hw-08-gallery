3d02d9010f0b5995cdb0cec8fa6ffc5ab7bcf0bb
Vlad Deputat <deputatwladek@gmail.com>
2021-03-07 18:58:22 +0200

    arrows added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import images from './js/gallery-items';
 import './css/styles.css';
 
 const galleryRef = document.querySelector('.js-gallery');
-
 const modalRef = document.querySelector('.js-lightbox');
 const modalBtnRef = document.querySelector('[data-action="close-lightbox"]');
 const modalImgRef = document.querySelector('.lightbox__image');
@@ -10,13 +9,14 @@ const modalOverlayRef = document.querySelector('.lightbox__overlay');
 
 function createGalleryMarkup(images) {
   return images
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, index) => {
       return `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
         <img
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
+            data-index=${index}
             alt="${description}"
           />
           </a>
@@ -28,6 +28,8 @@ function createGalleryMarkup(images) {
 const markup = createGalleryMarkup(images);
 galleryRef.insertAdjacentHTML('beforeend', markup);
 
+let currentIndex = 0;
+
 function openModal(e) {
   if (!e.target.classList.contains('gallery__image')) {
     return;
@@ -36,21 +38,32 @@ function openModal(e) {
   modalRef.classList.add('is-open');
   modalImgRef.src = e.target.dataset.source;
   modalImgRef.alt = e.target.alt;
-  document.addEventListener('keydown', escapeKey);
+  currentIndex = Number(e.target.dataset.index);
+  document.addEventListener('keydown', pressKey);
+  modalBtnRef.addEventListener('click', closeModal);
+  modalOverlayRef.addEventListener('click', closeModal);
 }
 
 function closeModal() {
   modalRef.classList.remove('is-open');
   modalImgRef.src = '';
   modalImgRef.alt = '';
-  document.removeEventListener('keydown', escapeKey);
-}
-
-function escapeKey(e) {
-  if (e.keyCode === 27) closeModal();
+  document.removeEventListener('keydown', pressKey);
+  modalBtnRef.removeEventListener('click', closeModal);
+  modalOverlayRef.removeEventListener('click', closeModal);
 }
 
 galleryRef.addEventListener('click', openModal);
 
-modalBtnRef.addEventListener('click', closeModal);
-modalOverlayRef.addEventListener('click', closeModal);
+function pressKey(e) {
+  if (e.keyCode === 27) closeModal();
+  if (e.keyCode === 37) {
+    if (currentIndex === 0) return;
+    currentIndex -= 1;
+  }
+  if (e.keyCode === 39) {
+    if (currentIndex === images.length - 1) return;
+    currentIndex += 1;
+  }
+  modalImgRef.src = images[currentIndex].original;
+}
